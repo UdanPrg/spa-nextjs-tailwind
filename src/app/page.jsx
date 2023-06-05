14c7c66536f018593368e17feb06a2c4a78f0ff8
UdanPrg <udan1718@gmail.com>
2023-06-05 11:58:46 -0600

    spa success

---
+++ src/app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex justify-center rounded-none h-screen items-center'>
+      <h1 className='rounded'>
+        Hola Mundo!
+      </h1>
+          
+    </main>
+  )
+}
---
+++ src/components/component.jsx
@@ -0,0 +1,7 @@
+export default function Cp(){
+    return(
+        <>
+        <div className=""></div>
+        </>
+    )
+}
